4886ed41d654b6b61493578fdac70b984794d67a
a1b0r <a1b0@pm.me>
2021-11-28 19:32:13 +0100

    cl

--- src/Action/UserListAction.php
+++ src/Action/UserListAction.php
@@ -40,20 +40,8 @@ final class UserListAction
         ResponseInterface $response,
         array $args = []
     ): ResponseInterface {
-        // Invoke the Domain (application service) with inputs and keep the result
         $users = $this->userLister->getUserList();
-
-        // // Transform the result into the JSON representation
-        // $result = [
-        //     'user_id' => $user->id,
-        //     'first_name' => $user->firstName,
-        //     'last_name' => $user->lastName,
-        //     'active' => $user->active,
-        // ];
-
-        // Build the HTTP response
         $response->getBody()->write((string)json_encode($users));
-
         return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
     }
 }
